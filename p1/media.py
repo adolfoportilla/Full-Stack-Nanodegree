2d5172d96477ea098fa5db5eb0cbf839b3efbb14
adolfoportilla <adolfoportilla@tamu.edu>
2017-07-06 18:52:03 -0700

    modified code based on review

--- p1/media.py
+++ p1/media.py
@@ -1,9 +1,16 @@
 import webbrowser
+
+
+#  Movie CLass
 class Movie():
-	def __init__(self,movie_title, movie_storyline, poster_image, trailer_youtube):
+    """Movie class to add movies"""
+    #  Class constructor
+	def __init__(self, movie_title, movie_storyline, poster_image, trailer):
 		self.title = movie_title
 		self.storyline = movie_storyline
 		self.poster_image_url = poster_image
-		self.trailer_youtube_url = trailer_youtube
+		self.trailer_youtube_url = trailer
+
+	# Helper method
 	def show_trailer(self):
-		webbrowser.open(self.trailer_youtube_url)		
+		webbrowser.open(self.trailer_youtube_url)
